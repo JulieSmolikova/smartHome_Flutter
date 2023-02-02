0a59edc049a782c69a7634bb647e57e92a98a7cb
Julie <julie_sua@mail.ru>
2023-02-02 13:40:47 +0200

    tempetature2

--- lib/painter_temp_box.dart
+++ lib/painter_temp_box.dart
@@ -9,7 +9,7 @@ class PainterTempBox extends CustomPainter {
     var paint = Paint()
       ..color = Colors.white70
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 2
+      ..strokeWidth = 1.5
       ..isAntiAlias = true
       ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 1);
 
@@ -18,9 +18,19 @@ class PainterTempBox extends CustomPainter {
         size.width * 0.58, size.height * 0.18,
         size.width * 0.68, size.height * 0.27);
 
+    path.moveTo(size.width * 0.32, size.height * 0.27);
+    path.cubicTo(size.width * 0.42, size.height * 0.16,
+        size.width * 0.56, size.height * 0.16,
+        size.width * 0.68, size.height * 0.27);
+
     path.moveTo(size.width * 0.38, size.height * 0.88);
     path.cubicTo(size.width * 0.43, size.height * 0.82,
         size.width * 0.57, size.height * 0.82,
+        size.width * 0.62, size.height * 0.88);
+
+    path.moveTo(size.width * 0.38, size.height * 0.88);
+    path.cubicTo(size.width * 0.45, size.height * 0.8,
+        size.width * 0.55, size.height * 0.8,
         size.width * 0.62, size.height * 0.88);
 
     canvas.drawPath(path, paint);
